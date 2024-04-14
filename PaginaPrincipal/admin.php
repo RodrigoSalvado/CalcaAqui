a67efa31f57e111884d377982feeed94e0108638
Kirlitos <rodrigoapsalvado@gmail.com>
2024-04-14 18:31:58 +0100

    include() para a bd

--- PaginaPrincipal/admin.php
+++ PaginaPrincipal/admin.php
@@ -1,16 +1,5 @@
 <?php // Ligação à bd
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-$conn = new mysqli($servername, $username, $password);
-mysqli_select_db($conn , 'CalcaAqui');
-
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}else{
-    echo "Entrou na bd <hr>";
-}
+include("../basedados/db.h");
 ?>
 <!DOCTYPE html>
 <html>
