3ea76dc56eb26b26274e92182db0c0bf98dbcab6
Friozinho <luis.santos.1317@gmail.com>
2024-05-18 18:07:13 +0100

    .

--- PaginaPrincipal/confirmarLogin.php
+++ PaginaPrincipal/confirmarLogin.php
@@ -9,7 +9,7 @@
             $username = mysqli_real_escape_string($conn, $_POST['username']);
             $password = $_POST['password'];
     
-            // Consulta para buscar a senha e tipo de usuário do nome de usuário inserido
+            
             $sqlSelect = "SELECT password, tipo_utilizador FROM conta WHERE username = '$username'";
             $resultSelect = mysqli_query($conn, $sqlSelect);
     
@@ -18,15 +18,15 @@
                 $hashed_password = $row['password'];
                 $tipo_user = $row['tipo_utilizador'];
     
-                // Confirmação de senha
+                
                 if (md5($password) == $hashed_password) {
-                    // Armazena as informações do usuário na sessão
+                    
                     $_SESSION['user'] = [
                         'nome' => $username,
                         'tipo_user' => $tipo_user
                     ];
     
-                    // Redireciona com base no tipo de usuário
+                    
                     if ($tipo_user == 1) {
                         header("Location: admin.php");
                     } elseif ($tipo_user == 2) {
@@ -34,7 +34,7 @@
                     } else {
                         echo "<script>window.alert('Dados de login inválidos!');</script>";
                     }
-                    exit(); // Certifique-se de sair após o redirecionamento
+                    exit(); 
                 } else {
                     echo "<script>window.alert('Senha inválida!');</script>";
                 }
